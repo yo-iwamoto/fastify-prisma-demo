5f76970cdec2e0fbd0b54d0a06249ba8547b0e70
you-5805 <you.iwamoto918@gmail.com>
2021-10-16 01:33:00 +0900

    add: prettier

---
+++ frontend/src/pages/index.tsx
@@ -0,0 +1,7 @@
+import { NextPage } from 'next';
+
+const Page: NextPage = () => {
+  return <div>Hello, Next.js</div>;
+};
+
+export default Page;
---
+++ frontend/tailwind.config.js
@@ -0,0 +1,4 @@
+module.exports = {
+  purge: ['./src/**/*.tsx'],
+  darkMode: false,
+}
